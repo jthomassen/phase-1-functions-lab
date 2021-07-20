083c5c1ec1814f06c4a9fb2da9313606619dd858
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-20 11:06:39 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const headQuarter = 42
+
+function distanceFromHqInBlocks(currentLocation) {
+    return Math.abs(headQuarter - currentLocation);
+}
+
+function distanceFromHqInFeet(currentLocation) {
+    return Math.abs((headQuarter - currentLocation)*264);
+}
+
+function distanceTravelledInFeet(currentLocation, dropOff) {
+    return Math.abs((dropOff - currentLocation)*264);
+}
+
+function calculatesFarePrice(currentLocation, dropOff) {
+    const price = distanceTravelledInFeet(currentLocation, dropOff);
+    if (price <= 400) {
+        return 0;
+    } else if (price > 400 && price <= 2000) {
+        return (price-400)*0.02;
+    } else if (price > 2000 && price <= 2500) {
+        return 25;
+    } else if (price > 2500) {
+        return 'cannot travel that far';
+    }
+
+}
